d136593cb16442d4a4854d4b0e2c8e6811974eb8
Claude Ni <Claudenij2@gmail.com>
2024-07-06 17:24:31 +0300

    ex1 done

---
+++ script/classes.js
@@ -0,0 +1,17 @@
+function addClass() {
+  const text = document.querySelector("#ex1");
+  const cls = document.querySelector("#class-input");
+  text.classList.add(cls.value);
+}
+
+function toggleVisibility(elemID) {
+  const elem = document.getElementById(elemID);
+  elem.classList.toggle("hidden");
+  elem.classList.toggle("visible");
+}
+
+function clickButton(btn) {
+  btn.classList.remove("default");
+  btn.classList.add("clicked");
+  btn.text = "Clicked!";
+}
